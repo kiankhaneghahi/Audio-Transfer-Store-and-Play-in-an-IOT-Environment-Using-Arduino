0c1ee826d290329115447da487ce181f39ba19d7
kiankhaneghahi <42879334+kiankhaneghahi@users.noreply.github.com>
2020-09-07 18:20:10 +0430

    Add files via upload

---
+++ UCTA2017 Project/Due_UART3_Serial_and_USB_Serial.ino
@@ -0,0 +1,85 @@
+
+
+#include <DueFlashStorage.h>
+DueFlashStorage dueFlashStorage;
+long i=0;
+uint8_t data = 0;
+long address=0;
+const int pin=66;   //DAC0
+double mul=0.8;
+
+
+void setup(){
+
+  Serial.begin(9600);
+  Serial3.begin(9600);
+  analogWriteResolution(8);
+  String c;
+}
+
+void loop() {
+  if (Serial3.available())
+  {
+  if (Serial3.readStringUntil('\n')=="d")
+  {
+
+ 
+  {   
+    /* NodeMCU USART Recieve Commands and Store in a Character Variable */
+    
+ 
+   
+    if(Serial.available())
+    {
+      Serial.write ('S');
+      for(long i=0; i<80000; i++)
+      {
+      data = Serial.read();
+      dueFlashStorage.write(address,data);
+      address++;
+      Serial.write('N');
+      }
+    }
+    }
+  }
+    else if (Serial3.readStringUntil('\n')=="p")
+    {
+    for(long i=0; i<80000; i++)
+      {
+        if(Serial3.readStringUntil('\n')=="s")
+        {
+        address=0;
+        }
+      
+    
+   else if (Serial3.readStringUntil('\n')=="a")
+    {
+      //set vol 100
+     mul=1;
+    }
+     else if (Serial3.readStringUntil('\n')=="b")
+    {
+      //set vol 50
+    mul=0.5;
+    } 
+   else if (Serial3.readStringUntil('\n')=="c")
+    {
+      //set vol 0
+      mul=0;
+    }
+      else 
+      {
+        byte dac = dueFlashStorage.read(address);
+        analogWrite(pin, dac*mul ); 
+        address++;
+        delayMicroseconds(123); // evaluating DAC write time without Timer interupt 
+      }
+    }
+     
+       
+  } 
+}
+}
+  
+
+
---
+++ UCTA2017 Project/ESP8266_Configuration_-_different_HTML_2.ino
@@ -0,0 +1,128 @@
+#include <ESP8266WiFi.h>
+
+
+const char* ssid = "Kian Khaneghahi";
+const char* password = "Kian123456@";
+
+WiFiServer server(80);
+
+int output_pin = 2;
+
+void setup() {
+
+ 
+
+  
+  Serial.begin(9600);
+  delay(10);
+
+  pinMode(output_pin, OUTPUT);
+  digitalWrite(output_pin, 0);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  server.begin();
+  Serial.println("Server started");
+
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+
+  while (!client.available()) {
+  delay(1);
+  }
+
+  String req = client.readStringUntil('\r');
+  //Serial.println(req);
+
+  if (req.indexOf("/Download") != -1) {
+  //digitalWrite(output_pin, 0);
+  Serial.println('d');
+     
+  }
+  else if (req.indexOf("/Play") != -1) {
+  digitalWrite(output_pin, 0);
+  Serial.println('p');
+     
+  }
+  else if (req.indexOf("/Stop") != -1) {
+  //digitalWrite(output_pin, 0);
+  Serial.println('s');
+
+  }
+  else if (req.indexOf("/Volume100") != -1) {
+  //digitalWrite(output_pin, 0);
+  Serial.println('a');
+     
+  }
+  else if (req.indexOf("/Volume50") != -1) {
+  //digitalWrite(output_pin, 0);
+  Serial.println('b');
+
+  }
+  else if (req.indexOf("Volume0") != -1) {
+  //digitalWrite(output_pin, 0);
+  Serial.println('c');
+  }
+  
+
+    client.flush();
+ 
+  
+  /*SERVER HTML CODE */
+  
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+
+  
+  s += "<head>";
+  s += "<meta charset=\"utf-8\">";
+  s += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  s += "<script src=\"https://code.jquery.com/jquery-2.1.3.min.js\"></script>";
+  s += "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css\">";
+  s += "</head>";
+  
+  s += "<div class=\"container\">";
+  s += "<h1>  In The Name Of God </h1>";
+  s += "<h1>  Media Control : </h1>";
+  s += "<div class=\"row\">";
+  s += "<div class=\"col-md-2\"><input class=\"btn btn-block btn-lg btn-success\"style=\"font-size:24px;border-radius:100%;height:100px;width:200px;\"type=\"button\" value=\"Download\" onclick=\"Download()\"></div>";
+  s += "<div class=\"col-md-2\"><input class=\"btn btn-block btn-lg btn-success\"style=\"font-size:24px;border-radius:100%;height:100px;width:200px;\" type=\"button\" value=\"Play\" onclick=\"Play()\"></div>";
+  s += "<div class=\"col-md-2\"><input class=\"btn btn-block btn-lg btn-success\"style=\"font-size:24px;border-radius:100%;height:100px;width:200px;\" type=\"button\" value=\"Stop\" onclick=\"Stop()\"></div>";
+  s += "<div class=\"col-md-2\"><input class=\"btn btn-block btn-lg btn-danger\"style=\"font-size:24px;border-radius:100%;height:100px;width:200px;\" type=\"button\" value=\"Volume100\" onclick=\"Volume100()\"></div>";
+  s += "<div class=\"col-md-2\"><input class=\"btn btn-block btn-lg btn-danger\"style=\"font-size:24px;border-radius:100%;height:100px;width:200px;\" type=\"button\" value=\"Volume50\" onclick=\"Volume50()\"></div>";
+  s += "<div class=\"col-md-2\"><input class=\"btn btn-block btn-lg btn-danger\"style=\"font-size:24px;border-radius:100%;height:100px;width:200px;\" type=\"button\" value=\"Volume0\" onclick=\"Volume0()\"></div>";
+  s += "</div></div>";
+  
+
+  s += "<script>function Download() {$.get(\"/Download\");}</script>";
+  s += "<script>function Play() {$.get(\"/Play\");}</script>";
+  s += "<script>function Stop() {$.get(\"/Stop\");}</script>";
+  s += "<script>function Volume100() {$.get(\"/Volume100\");}</script>";
+  s += "<script>function Volume50() {$.get(\"/Volume50\");}</script>";
+  s += "<script>function Volume0() {$.get(\"/Volume0\");}</script>";
+  
+
+
+  client.print(s);
+  delay(1);
+    
+  //Serial.println("Client disconnected");
+}
